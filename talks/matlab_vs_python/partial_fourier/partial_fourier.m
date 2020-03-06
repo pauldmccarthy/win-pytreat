54c1373b51c264aa4d882f0eb2bbd8c938622382
Mark Chiew <mchiew@fmrib.ox.ac.uk>
2020-03-06 23:04:57 +0000

    Updated bloch/partial_fourier, more comments

--- talks/matlab_vs_python/partial_fourier/partial_fourier.m
+++ talks/matlab_vs_python/partial_fourier/partial_fourier.m
@@ -1,32 +1,84 @@
-%% Imports
-
 %% Load Data
+% get matfile object
 h = matfile('data.mat');
+
+% get img variable from the mat-file
 img = h.img;
 
 %% 6/8 Partial Fourier sampling
+% generate normally-distributed complex noise
 n = randn(96) + 1j*randn(96);
-y = fftshift(fft2(img),1) + 0*n;
-%y(73:end,:) = 0;
+
+% Fourier transform the image and add noise
+y = fftshift(fft2(img),1) + n;
+
+% set bottom 24/96 lines to 0
+y(73:end,:) = 0;
 
 %% Estimate phase
-pad = padarray(hann(48),24);
-phs = exp(1j*angle(ifft2(ifftshift(y.*pad,1))));
+% create zero-padded hanning filter for ky-filtering
+filt = padarray(hann(48),24);
+
+% generate low-res image with inverse Fourier transform
+low = ifft2(ifftshift(y.*filt,1));
+
+% get phase image
+phs = exp(1j*angle(low));
 
 %% POCS reconstruction
+% initialise image estimate to be zeros
 est = zeros(96);
+
+% set number of iterations
 iters = 10;
+
+% each iteration cycles between projections
 for i = 1:iters
-    est = fftshift(fft2(est.*phs),1);
+% projection onto data-consistent set:
+    % use a-priori phase to get complex image
+    est = est.*phs;
+
+    % Fourier transform to get k-space
+    est = fftshift(fft2(est), 1);
+
+    % replace data with measured lines
     est(1:72,:) = y(1:72,:);
-    est = max(real(ifft2(ifftshift(est,1)).*conj(phs)),0);   
+
+    % inverse Fourier transform to get back to image space
+    est = ifft2(ifftshift(est, 1));
+
+% projection onto non-negative reals
+    % remove a-priori phase
+    est = est.*conj(phs);
+
+    % get real part
+    est = real(est);
+
+    % ensure output is non-negative
+    est = max(est, 0);
 end
 
-%% Plot reconstruction
+%% Display error and plot reconstruction
+% compute zero-filled recon
+zf = ifft2(ifftshift(y, 1));
+
+% compute rmse for zero-filled and POCS recon
+err_zf = norm(zf(:) - img(:));
+err_pocs = norm(est(:).*phs(:) - img(:));
+
+% print errors
+fprintf(1, 'RMSE for zero-filled recon: %f\n', err_zf);
+fprintf(1, 'RMSE for POCS recon: %f\n', err_pocs);
+
+% plot both recons side-by-side
 figure();
+
+% plot zero-filled
 subplot(1,2,1);
-imshow(abs(ifft2(ifftshift(y,1))),[0 1],'colormap',jet);
+imshow(abs(zf), [0 1]);
 title('Zero-Filled');
+
+% plot POCS
 subplot(1,2,2);
-imshow(est,[0 1],'colormap',jet);
+imshow(est, [0 1]);
 title('POCS recon');
